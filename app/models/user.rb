9dd5103d3c335396078c6407bfec4de74028b713
ikenatsu <kei.trmc.s2-2sc@outlook.jp>
2024-01-16 14:03:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :age, presence: true
+
+  validates :gender_id, :occupation_id, :family_id,
+            numericality: { other_than: 1, message: 'must be selected' }
+
 end
